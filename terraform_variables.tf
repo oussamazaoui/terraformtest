f385c21a775cfb856a0e62d82a854d6f5fb69eaf
Oussama Zaoui <133207079+oussamazaoui@users.noreply.github.com>
2025-02-09 18:03:19 +0100

    Update terraform_variables.tf

--- terraform_variables.tf
+++ terraform_variables.tf
@@ -1,13 +1,13 @@
-variable "client_id" {
+variable "ClientID" {
   description = "The Client ID for the Service Principal."
   type        = string
 }
-variable "client_secret" {
+variable "CLIENT_SECRET" {
   description = "The Client Secret for the Service Principal."
   type        = string
 }
 
-variable "tenant_id" {
+variable "TENANT_ID" {
   description = "The Tenant ID for Azure."  
   type        = string
 }
